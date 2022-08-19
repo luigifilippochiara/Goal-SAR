d7069214501b358d7f14c8526f7fe1155b0aaf23
LuigiFilippoChiara <luigifilippochiara@gmail.com>
2022-08-19 19:47:00 +0200

    New raw data

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://www.dropbox.com/s/980xn344luntr4s/dataset.zip?dl=0 >> data.zip
+curl -L https://www.dropbox.com/s/luu2t6c6d24xvrb/dataset.zip?dl=0 >> data.zip
 unzip data.zip
 rm -r data.zip
 mv data ..
